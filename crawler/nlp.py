e18789996d912f3bd77314fdf830f06db3b25e58
A-Nian <97599669+anian5005@users.noreply.github.com>
2022-07-04 14:19:51 +0800

    Feat: movie name matcher function to check if two movies are the same by string preprocess and pyhton difflib package

---
+++ crawler/nlp.py
@@ -0,0 +1,25 @@
+import string
+from import SequenceMatcher
+
+# s1 = 'top gun'
+# s2 = 'top gun:'
+# s3 = '今天天氣真好'
+
+
+def match_preprocessing(movie_1, movie_2):
+
+
+    def preprocess(str):
+        text = str.lower()
+        for char in string.punctuation:
+            text = text.replace(char, " ")
+            return text
+
+    movie_1 = preprocess(movie_1)
+    movie_2 = preprocess(movie_2)
+    ratio = SequenceMatcher(lambda x: x in " ", movie_1, movie_2).ratio()
+    return ratio
+
+
+# r = match_preprocessing(s1, s2)
+# print(r)
