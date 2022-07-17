8020e6fdad2f0877bc3f9e15ef7cbd8e7ec10f88
A-Nian <97599669+anian5005@users.noreply.github.com>
2022-07-17 14:16:37 +0800

    [PYTHON][FIX] Fix slider bug which create duplicate div-box in home page when user click many time in a short time, and fix it by group two action of clear box and create box

--- static/js/home.js
+++ static/js/home.js
@@ -1,17 +1,29 @@
+console.log('home js')
+function clear_box() {
+        var node = document.getElementById('container');
+        node.remove();
+
+        var div_el= document.createElement('div');
+        div_el.classList.add('class', 'container');
+        div_el.setAttribute("id", 'container');
+
+        var parent = document.getElementsByClassName('article')[0];
+        parent.appendChild(div_el);
+}
 
 
+//home page default movie
 function movie_list(){
     var dataUrl= "/api/movielist"
     var xhr = new XMLHttpRequest()
     xhr.open('GET',dataUrl, true)
     xhr.send()
-    
-    
+
     xhr.onload = function(){
         if(xhr.status == 200){
             console.log('xhr.status', xhr.status);
             var data = JSON.parse(this.responseText);
-            console.log(data);
+//            console.log(data);
             create_box(data);
 
         }
@@ -24,40 +36,48 @@ movie_list()
 function create_box(data){
 
 //    a_outer > div_el > img
+//        console.log('data', data);
+        var data_list = data['data'];
+//        console.log('data_list', data_list);
+        for (var i = 0; i < data_list.length; i++) {
+            // data
+            var movie = data_list[i];
+    //         console.log(movie);
 
-    var data_list = data['data'];
-//    console.log('data_list', data_list)
-    for (var i = 0; i < data_list.length; i++) {
-        // data
-        var movie = data_list[i];
-//         console.log(movie);
-
-        // a_outer
-        var merge_id = movie['merge_id'];
-        var a_outer = document.createElement('a');
-        var page_url = "/title/" + merge_id
-         a_outer.setAttribute('href', page_url);
-         a_outer.setAttribute('target', "_blank");
+            // a_outer
+            var merge_id = movie['merge_id'];
+            var a_outer = document.createElement('a');
+            var page_url = "/title/" + merge_id
+             a_outer.setAttribute('href', page_url);
+             a_outer.setAttribute('target', "_blank");
 
 
-        // div_el
-        var div_el= document.createElement('div');
-        div_el.classList.add('class', 'box');
+            // div_el
+            var div_el= document.createElement('div');
+            div_el.classList.add('class', 'box');
 
-        // img_el
-        var image = movie['image'];
-        var img_el = document.createElement('img');
-        img_el.setAttribute('src', image);
+            // img_el
+            var image = movie['image'];
+            var img_el = document.createElement('img');
+            img_el.setAttribute('src', image);
 
-        // assemble
-        div_el.appendChild(img_el);
-        a_outer.appendChild(div_el);
+            // assemble
+            div_el.appendChild(img_el);
+            a_outer.appendChild(div_el);
 
-        document.getElementById('container').appendChild(a_outer);
-    }
+            document.getElementById('container').appendChild(a_outer);
+        }
+
 }
 
-function filter_movie_list(year_min, year_max){
+function create_no_result(){
+    var result_msg = document.createElement('h3');
+    var text = document.createTextNode("Sorry, no results found for this range");
+    result_msg.appendChild(text);
+    document.getElementById('container').appendChild(result_msg);
+}
+
+function update_data_by_slider(year_min, year_max){
     var dataUrl= "/api/movielist/" + year_min + '/' +year_max
     var xhr = new XMLHttpRequest()
     xhr.open('GET',dataUrl, true)
@@ -68,9 +88,16 @@ function filter_movie_list(year_min, year_max){
         if(xhr.status == 200){
             console.log('xhr.status', xhr.status);
             var data = JSON.parse(this.responseText);
-            console.log(data);
-            create_box(data);
+            clear_box();
+            console.log('create box >> data', data);
+            console.log(typeof data['data'])
 
+            if (data['data'] !== null) {
+                create_box(data);
+            } else {
+                create_no_result()
+                console.log('no data');
+            }
         }
     }
 }
--- static/js/home_slider.js
+++ static/js/home_slider.js
@@ -1,3 +1,4 @@
+console.log('home slider JS')
 const range = () => {
     var inputLeft = document.getElementById("input-left");
     var inputRight = document.getElementById("input-right");
@@ -60,7 +61,7 @@ const range = () => {
     });
     inputLeft.addEventListener("mouseup", function() {
       thumbLeft.classList.remove("active");
-      get_year_filter()
+      get_new_movie()
     });
 
     inputRight.addEventListener("mouseover", function() {
@@ -75,87 +76,87 @@ const range = () => {
     });
     inputRight.addEventListener("mouseup", function() {
       thumbRight.classList.remove("active");
-      get_year_filter()
+      get_new_movie()
     });
 };
 
 range();
 
 
-const range_IMDB = () => {
-    var inputLeft = document.getElementById("input-left_IMDB");
-    var inputRight = document.getElementById("input-right_IMDB");
-
-    var thumbLeft = document.querySelector(".multi-range-slider.IMDB > .slider.IMDB > .thumb.left");
-    var thumbRight = document.querySelector(".multi-range-slider.IMDB > .slider.IMDB > .thumb.right");
-    var range = document.querySelector(".multi-range-slider.IMDB > .slider.IMDB > .range");
-    var showeL = document.querySelector(".multi-range-slider.IMDB > .slider.IMDB > .left-s");
-    var showeR = document.querySelector(".multi-range-slider.IMDB > .slider.IMDB > .right-s");
-
-    function setLeftValue() {
-      var _this = inputLeft,
-        min = parseInt(_this.min),
-
-        max = parseInt(_this.max);
-//       console.log( _this.value);
-//          console.log( _this.value);
-
-      _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
-
-      var percent = ((_this.value - min) / (max - min)) * 100;
-
-      thumbLeft.style.left = percent + "%";
-      range.style.left = percent + "%";
-
-      showeL.style.left = percent + "%";
-      showeL.innerHTML = _this.value;
-    }
-    setLeftValue();
-
-    function setRightValue() {
-      var _this = inputRight,
-        min = parseInt(_this.min),
-        max = parseInt(_this.max);
-
-      _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
-
-      var percent = ((_this.value - min) / (max - min)) * 100;
-      thumbRight.style.right = (100 - percent) + "%";
-      range.style.right = (100 - percent) + "%";
-
-      showeR.style.right = (100 - percent) + "%";
-      showeR.innerHTML = _this.value;
-    }
-    setRightValue();
-
-    inputLeft.addEventListener("input", setLeftValue);
-    inputRight.addEventListener("input", setRightValue);
-
-    inputLeft.addEventListener("mouseover", function() {
-      thumbLeft.classList.add("hover");
-    });
-    inputLeft.addEventListener("mouseout", function() {
-      thumbLeft.classList.remove("hover");
-    });
-    inputLeft.addEventListener("mousedown", function() {
-      thumbLeft.classList.add("active");
-    });
-    inputLeft.addEventListener("mouseup", function() {
-      thumbLeft.classList.remove("active");
-    });
-
-    inputRight.addEventListener("mouseover", function() {
-      thumbRight.classList.add("hover");
-    });
-    inputRight.addEventListener("mouseout", function() {
-      thumbRight.classList.remove("hover");
-    });
-    inputRight.addEventListener("mousedown", function() {
-      thumbRight.classList.add("active");
-    });
-    inputRight.addEventListener("mouseup", function() {
-      thumbRight.classList.remove("active");
-    });
-};
-
-range_IMDB();
+//const range_IMDB = () => {
+//    var inputLeft = document.getElementById("input-left_IMDB");
+//    var inputRight = document.getElementById("input-right_IMDB");
+//
+//    var thumbLeft = document.querySelector(".multi-range-slider.IMDB > .slider.IMDB > .thumb.left");
+//    var thumbRight = document.querySelector(".multi-range-slider.IMDB > .slider.IMDB > .thumb.right");
+//    var range = document.querySelector(".multi-range-slider.IMDB > .slider.IMDB > .range");
+//    var showeL = document.querySelector(".multi-range-slider.IMDB > .slider.IMDB > .left-s");
+//    var showeR = document.querySelector(".multi-range-slider.IMDB > .slider.IMDB > .right-s");
+//
+//    function setLeftValue() {
+//      var _this = inputLeft,
+//        min = parseInt(_this.min),
+//
+//        max = parseInt(_this.max);
+////       console.log( _this.value);
+////          console.log( _this.value);
+//
+//      _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
+//
+//      var percent = ((_this.value - min) / (max - min)) * 100;
+//
+//      thumbLeft.style.left = percent + "%";
+//      range.style.left = percent + "%";
+//
+//      showeL.style.left = percent + "%";
+//      showeL.innerHTML = _this.value;
+//    }
+//    setLeftValue();
+//
+//    function setRightValue() {
+//      var _this = inputRight,
+//        min = parseInt(_this.min),
+//        max = parseInt(_this.max);
+//
+//      _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
+//
+//      var percent = ((_this.value - min) / (max - min)) * 100;
+//      thumbRight.style.right = (100 - percent) + "%";
+//      range.style.right = (100 - percent) + "%";
+//
+//      showeR.style.right = (100 - percent) + "%";
+//      showeR.innerHTML = _this.value;
+//    }
+//    setRightValue();
+//
+//    inputLeft.addEventListener("input", setLeftValue);
+//    inputRight.addEventListener("input", setRightValue);
+//
+//    inputLeft.addEventListener("mouseover", function() {
+//      thumbLeft.classList.add("hover");
+//    });
+//    inputLeft.addEventListener("mouseout", function() {
+//      thumbLeft.classList.remove("hover");
+//    });
+//    inputLeft.addEventListener("mousedown", function() {
+//      thumbLeft.classList.add("active");
+//    });
+//    inputLeft.addEventListener("mouseup", function() {
+//      thumbLeft.classList.remove("active");
+//    });
+//
+//    inputRight.addEventListener("mouseover", function() {
+//      thumbRight.classList.add("hover");
+//    });
+//    inputRight.addEventListener("mouseout", function() {
+//      thumbRight.classList.remove("hover");
+//    });
+//    inputRight.addEventListener("mousedown", function() {
+//      thumbRight.classList.add("active");
+//    });
+//    inputRight.addEventListener("mouseup", function() {
+//      thumbRight.classList.remove("active");
+//    });
+//};
+//
+//range_IMDB();
