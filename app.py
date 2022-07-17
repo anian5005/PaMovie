ea6cb1c493ba534f526142822915701b7b49f77a
A-Nian <97599669+anian5005@users.noreply.github.com>
2022-07-17 14:08:30 +0800

    [PYTHON][FIX] Reduce the time to query SQL DB for web movie data and add 404 page

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, redirect, url_for, request, flash, send_from_directory, jsonify, make_response
-from package.db import sql
-import time
+from flask import Flask, render_template
+from package.db.sql import all_zh_data_from_db, filter_zh_data_from_db, zh_data_from_db,movie_page_data,zh_cast_from_db
 
 app = Flask(__name__)
 
@@ -9,7 +8,7 @@ def movie_home():
     try:
         return render_template("home.html")
     except:
-        return render_template('404_error.html'), 404
+        return render_template('404.html'), 404
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -17,14 +16,14 @@ def page_not_found(e):
 
 @app.route('/api/movielist')
 def movie_list():
-    all_movie_list = sql.all_zh_data_from_db()
+    all_movie_list = all_zh_data_from_db()
     movie_list_json = {'data': all_movie_list}
     return movie_list_json
 
 
 @app.route('/api/movielist/<int:year_min>/<int:year_max>')
 def movie_list_by_year(year_min, year_max):
-    movie_list_year = sql.filter_zh_data_from_db(year_min, year_max)
+    movie_list_year = filter_zh_data_from_db(year_min, year_max)
 
     # print(movie_list)
     movie_list_json = {'data': movie_list_year}
@@ -33,62 +32,48 @@ def movie_list_by_year(year_min, year_max):
 @app.route('/api/info/<string:merge_id>')
 def movie_info(merge_id):
 
-    data = sql.zh_data_from_db(merge_id)
-    cast = sql.zh_cast_from_db(merge_id)
-    rating = sql.rating_from_db(merge_id)
+    data = zh_data_from_db(merge_id)
+    cast = zh_cast_from_db(merge_id)
+    # rating = sql.rating_from_db(merge_id)
     data.update(cast)
-    data.update(rating)
-
-
+    # data.update(rating)
     return data
 
+
+
+
+
 @app.route('/title/<string:merge_id>')
-# tt1745960
 def movie_page(merge_id):
     try:
-        start = time.time()
-        data = sql.zh_data_from_db(merge_id)
-        # print('data', data)
+        general, cast_dict = movie_page_data(merge_id)
 
-        cast = sql.zh_cast_from_db(merge_id)
-        # print('cast', cast)
+        movie = general['en_title']
+        zh_title = general['zh_title']
+        release_date = general['release_date']
+        image = general['image']
+        runtime = general['runtime']
+        imdb_score = general['imdb_score']
+        imdb_count = general['imdb_count']
+        tomato_meter = general['tomato_meter']
+        tomato_review_count = general['tomato_review_count']
+        tomato_audience = general['tomato_audience']
+        tomato_audience_count = general['tomato_audience_count']
+        yahoo_score = general['yahoo_score']
+        yahoo_count = general['yahoo_count']
 
-        rating = sql.rating_from_db(merge_id)
-        # print('rating', rating)
 
-        first = data
-        movie = first['en_title']
-        zh_title = first['zh_title']
-        release_date = first['release_date']
-        image = first['image']
-        runtime = first['runtime']
-
-
-        imdb_score =  rating['imdb_score']
-        imdb_count =rating['imdb_count']
-        tomato_meter = rating['tomato_meter']
-        tomato_review_count = rating['tomato_review_count']
-        tomato_audience = rating['tomato_audience']
-        tomato_audience_count = rating['tomato_audience_count']
-        yahoo_score= rating['yahoo_score']
-        yahoo_count= rating['yahoo_count']
-
-        # print('movie', first)
-
-        if cast.get('dir', None) != None:
-            dirs = cast['dir'][0]
+        if cast_dict.get('dir', None) != None:
+            dirs = cast_dict['dir'][0]
         else:
             dirs = 'no result'
-        end = time.time()
-        print('spent', end-start)
-        print('data', data)
 
         return render_template('movie_page.html',
                                movie=movie,
                                zh_title=zh_title,
                                release_date=release_date,
                                runtime=runtime,
-                               rating=rating,
+                               rating=general,
                                image=image,
                                dir=dirs,
                                imdb_score=imdb_score,
@@ -101,8 +86,6 @@ def movie_page(merge_id):
                                yahoo_count=yahoo_count
                                )
     except:
-        return render_template('404_error.html'), 404
+        return render_template('404.html'), 404
 
-
-# app.run(debug=True, port=8000, host="0.0.0.0", ssl_context=('ssl/certificate.crt', 'ssl/private.key'))
 app.run(debug=True, port=8000, host="0.0.0.0")
